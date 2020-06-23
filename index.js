b635b615dd6d3b4c6dc950cfa248efe13c22e3ff
Gamoundo <akilgg@gmail.com>
2020-06-23 09:38:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove();
+let newHeader = document.createElement("h1")
+document.body.appendChild(newHeader)
+// document.querySelector("newHeader").innerHTML = "<h1 id ='victory'>X is the Champion</h1>"
+newHeader.textContent = "Akil is the champion"
+newHeader.id = 'victory'
